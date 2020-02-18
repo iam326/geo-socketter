47577ea3f7388c63f4c7e6e60d8f945688584dbf
t.takahashi <takahashi.developer@gmail.com>
2020-02-18 23:24:18 +0900

    implement connect

--- backend/onconnect/main.go
+++ backend/onconnect/main.go
@@ -1,47 +1,43 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"io/ioutil"
-	"net/http"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
 var (
-	// DefaultHTTPGetAddress Default Address
-	DefaultHTTPGetAddress = "https://checkip.amazonaws.com"
-
-	// ErrNoIP No IP found in response
-	ErrNoIP = errors.New("No IP in HTTP response")
-
-	// ErrNon200Response non 200 status code in response
-	ErrNon200Response = errors.New("Non 200 Response found")
+	ddb = dynamodb.New(session.New(), aws.NewConfig().WithRegion("ap-northeast-1"))
 )
 
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	resp, err := http.Get(DefaultHTTPGetAddress)
+func handler(request events.APIGatewayWebsocketProxyRequest) (events.APIGatewayProxyResponse, error) {
+	tableName := os.Getenv("TABLE_NAME")
+	param := &dynamodb.PutItemInput{
+		TableName: aws.String(tableName),
+		Item: map[string]*dynamodb.AttributeValue{
+			"connectionId": {
+				S: aws.String(request.RequestContext.ConnectionID),
+			},
+		},
+	}
+
+	item, err := ddb.PutItem(param)
 	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
+		fmt.Println(err)
+		return events.APIGatewayProxyResponse{
+			Body:       fmt.Sprintf("Failed to connect: %v", string(err.Error())),
+			StatusCode: 500,
+		}, nil
 	}
-
-	if resp.StatusCode != 200 {
-		return events.APIGatewayProxyResponse{}, ErrNon200Response
-	}
-
-	ip, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
-	}
-
-	if len(ip) == 0 {
-		return events.APIGatewayProxyResponse{}, ErrNoIP
-	}
+	fmt.Println(item)
 
 	return events.APIGatewayProxyResponse{
-		Body:       fmt.Sprintf("Hello, %v", string(ip)),
+		Body:       "Connected.",
 		StatusCode: 200,
 	}, nil
 }
