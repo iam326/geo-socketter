6b4f99a8acdaf96d99a90546581f8ddeef103a63
t.takahashi <takahashi.developer@gmail.com>
2020-02-22 23:30:41 +0900

    implement sendmessage

--- backend/sendmessage/main.go
+++ backend/sendmessage/main.go
@@ -1,47 +1,93 @@
 package main
 
 import (
-	"errors"
+	"context"
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net/http"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/apigatewaymanagementapi"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
+
+// PostData ...
+type PostData struct {
+	Message string `json:"message"`
+	Data    string `data:"message"`
+}
+
+// Connection ...
+type Connection struct {
+	ConnectionID string `dynamodbav:"connectionId"`
+}
 
 var (
-	// DefaultHTTPGetAddress Default Address
-	DefaultHTTPGetAddress = "https://checkip.amazonaws.com"
-
-	// ErrNoIP No IP found in response
-	ErrNoIP = errors.New("No IP in HTTP response")
-
-	// ErrNon200Response non 200 status code in response
-	ErrNon200Response = errors.New("Non 200 Response found")
+	ddb = dynamodb.New(session.New(), aws.NewConfig().WithRegion("ap-northeast-1"))
 )
 
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	resp, err := http.Get(DefaultHTTPGetAddress)
+func handler(ctx context.Context, request events.APIGatewayWebsocketProxyRequest) (events.APIGatewayProxyResponse, error) {
+	tableName := os.Getenv("TABLE_NAME")
+	param := &dynamodb.ScanInput{
+		TableName:            aws.String(tableName),
+		ProjectionExpression: aws.String("connectionId"),
+	}
+
+	result, err := ddb.Scan(param)
 	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
+		fmt.Println(err)
+		return events.APIGatewayProxyResponse{
+			Body:       fmt.Sprintf("Failed to scan: %v", string(err.Error())),
+			StatusCode: 500,
+		}, nil
 	}
+	fmt.Println(result)
 
-	if resp.StatusCode != 200 {
-		return events.APIGatewayProxyResponse{}, ErrNon200Response
-	}
-
-	ip, err := ioutil.ReadAll(resp.Body)
+	var config *aws.Config
+	sess, err := session.NewSession(config)
 	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
+		fmt.Println(err)
+		return events.APIGatewayProxyResponse{
+			Body:       fmt.Sprintf("Failed to session: %v", string(err.Error())),
+			StatusCode: 500,
+		}, nil
 	}
 
-	if len(ip) == 0 {
-		return events.APIGatewayProxyResponse{}, ErrNoIP
+	svc := apigatewaymanagementapi.New(sess)
+	svc.Endpoint = fmt.Sprintf("https://%s/%s", request.RequestContext.DomainName, request.RequestContext.Stage)
+
+	var postData PostData
+	err = json.Unmarshal([]byte(request.Body), &postData)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       fmt.Sprintf("Failed to Unmarshal: %v", string(err.Error())),
+			StatusCode: 500,
+		}, nil
+	}
+
+	var connections []Connection
+	err = dynamodbattribute.UnmarshalListOfMaps(result.Items, &connections)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       fmt.Sprintf("Failed to UnmarshalList: %v", string(err.Error())),
+			StatusCode: 500,
+		}, nil
+	}
+
+	for _, conn := range connections {
+		connectionID := conn.ConnectionID
+		svc.PostToConnection(&apigatewaymanagementapi.PostToConnectionInput{
+			ConnectionId: &connectionID,
+			Data:         []byte(postData.Data),
+		})
 	}
 
 	return events.APIGatewayProxyResponse{
-		Body:       fmt.Sprintf("Hello, %v", string(ip)),
+		Body:       "Message Sent.",
 		StatusCode: 200,
 	}, nil
 }
