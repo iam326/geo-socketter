59774d5c865ea3e28fd99f144048533cd84a85a2
t.takahashi <takahashi.developer@gmail.com>
2020-03-03 23:53:44 +0900

    connect websocket server

--- frontend/src/sagas/chat.ts
+++ frontend/src/sagas/chat.ts
@@ -1,15 +1,35 @@
-import { put, takeEvery, delay } from 'redux-saga/effects';
+import io from 'socket.io-client';
+import { all, call, fork } from 'redux-saga/effects';
 
-import { actions } from '../actions/chat';
+const url = 'URL'
+function connect() {
+  const socket = io(url, {
+    path: `/Prod`,
+    transports: [
+      'websocket',
+      'polling',
+      'flashsocket'
+    ]
+  });
+  return new Promise(resolve => {
+    socket.on('connect', () => {
+      resolve(socket);
+    });
+  });
+}
 
-export type SendMessageStarted = ReturnType<typeof actions.sendMessageActions.started>
-
-export function* async(action: SendMessageStarted) {
-  yield put(actions.sendMessageActions.started(action.payload));
-  yield delay(1000);
-  yield put(actions.sendMessageActions.done({params: action.payload}));
+function* watchOnSocket() {
+  while (true) {
+    try {
+      yield call(connect)
+    } catch (e) {
+      console.error('socket: error:', e);
+    }
+  }
 }
 
 export default function* rootSaga() {
-  yield takeEvery(actions.sendMessage, async)
+  yield all([
+    fork(watchOnSocket)
+  ])
 }
