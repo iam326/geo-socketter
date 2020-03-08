bab8b3e750fce584b3d37b6382e23cae24903597
t.takahashi <takahashi.developer@gmail.com>
2020-03-08 12:06:05 +0900

    add google maps api key

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -8,6 +8,7 @@ readonly STACK_NAME="${PROJECT_NAME}-backend"
 readonly TEMPLATE_FILE="$(pwd)/template.yaml"
 readonly CONNECTIONS_TABLE_NAME="${PROJECT_NAME}-connections"
 readonly WEB_SOCKET_NAME="${PROJECT_NAME}-websocket"
+readonly GOOGLE_MAPS_API_PARAM_NAME="/${PROJECT_NAME}/GoogleMapsAPIKey"
 
 make build
 
@@ -31,6 +32,14 @@ WEB_SOCKET_URI=$(aws cloudformation describe-stacks \
   --query 'Stacks[].Outputs[?OutputKey==`WebSocketURI`].OutputValue' \
   --output text)
 
+aws ssm put-parameter \
+  --name ${GOOGLE_MAPS_API_PARAM_NAME} \
+  --value ${GOOGLE_MAPS_API_KEY} \
+  --type SecureString \
+  --no-overwrite
+# TODO: put済みの場合は ssm get-parameter で apikey を取得する
+
 echo "
 REACT_APP_WEB_SOCKET_URI=${WEB_SOCKET_URI}
+REACT_APP_GOOGLE_MAPS_API_KEY=${GOOGLE_MAPS_API_KEY} 
 " > ../frontend/.env
