cce215a7f27a5e35651278fe3fcebe99179c3778
t.takahashi <takahashi.developer@gmail.com>
2020-04-06 22:58:20 +0900

    add a form dialog to create a new room

--- frontend/src/components/RoomList.tsx
+++ frontend/src/components/RoomList.tsx
@@ -6,6 +6,15 @@ import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
 import ChatIcon from '@material-ui/icons/Chat';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 import Header from './Header';
 
@@ -15,6 +24,11 @@ const useStyles = makeStyles((theme) => ({
     maxWidth: 360,
     backgroundColor: theme.palette.background.paper,
   },
+  fab: {
+    position: 'absolute',
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+  }
 }));
 
 interface Props {
@@ -40,6 +54,15 @@ function RoomItem(props: Props) {
 
 export default function RoomList() {
   const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <React.Fragment>
@@ -49,6 +72,38 @@ export default function RoomList() {
         <RoomItem title="タイトル" subTitle="サブタイトル" />
         <RoomItem title="タイトル" subTitle="サブタイトル" />
       </List>
+      <Fab
+        className={classes.fab}
+        color="primary"
+        aria-label="add"
+        onClick={handleClickOpen}
+      >
+        <AddIcon />
+      </Fab>
+      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">Create a new room</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To create a new room, enter the room name here.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Room Name"
+            type="text"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleClose} color="primary">
+            Create
+          </Button>
+        </DialogActions>
+      </Dialog>
     </React.Fragment>
   );
 }
